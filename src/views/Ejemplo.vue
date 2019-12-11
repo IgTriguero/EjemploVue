949a3fa545eaaeb9f06e3e36eb4e9b14d1a0febf
IgTriguero <itrigueropedraza@gmail.com>
2019-12-11 20:06:06 +0100

    Comentado todo

--- src/views/Ejemplo.vue
+++ src/views/Ejemplo.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-      <p>{{variable}}</p>
-      <p>{{inputText}}</p>
-      <p v-if="inputText === 'xd'">Ja, xD</p>
+      <p>{{variable}}</p> <!-- imprime la variable "variable" -->
+      <p>{{inputText}}</p> <!-- imprime la variable "inputText" -->
+      <p v-if="inputText === 'xd'">Ja, xD</p> <!-- se imprime solo si "inputText" es xd -->
       
-      <input type="text" v-model="inputText">
+      <input type="text" v-model="inputText"> <!-- cambia automaticamente la variable "inputText" -->
 
-      <p v-for="(item, index) in array" :key="index">{{item}}</p>
-      <button @click="funtion1">Exec func 1</button>
-      <button @click="funtion2">Exec fucn 2</button>
+      <p v-for="(item, index) in array" :key="index">{{item}}</p> <!-- recorre el array "array" -->
+
+      <button @click="funtion1">Exec func 1</button> <!-- ejecuta la "funtion1" -->
+      <button @click="funtion2">Exec func 2</button> <!-- ejecuta la "funtion2" -->
   </div>
 
 </template>
@@ -16,19 +17,27 @@
 <script>
 export default {
   name: "Ejemplo",
-  data(){
+  data(){ //donde se guardan todas las variables
     return{
       variable: "Que pasa gente, estoy DORAMION",
       inputText: "",
       array:["hola", "hola1", "this."]
     }
   },
-  methods: {
-    funtion2(){
+  methods: { //donde se guardan todos los metodos
+    funtion2(){//cambia la variable "variable" a "funtion2"
       this.variable = "funtion2"
     },
-    funtion1(){
+    funtion1(){//cambia la variable "variable" a "funtion1"
       this.variable = "funtion1"
+    }
+  },
+  computed: { //funciones para editar variables sin perder la variable original
+
+  }, 
+  watch:{ //para ejecutar funciones cuando cambie alguna variable
+    variable: function () { //imprime por consola cada vez que se cambia la variable "variable"
+      console.log("Variable cambiada")
     }
   }
 }
